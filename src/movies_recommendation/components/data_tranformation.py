d6f8a5e50acb3c87874bd41c11320c8c53268ebc
Amantyagidatascientist <tyagiaman3558@gmail.com>
2024-08-25 15:40:40 +0530

    complite preprosiing:

--- src/movies_recommendation/components/data_tranformation.py
+++ src/movies_recommendation/components/data_tranformation.py
@@ -7,10 +7,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.sparse import csr_matrix
 from src.movies_recommendation.utils import save_object
 from src.movies_recommendation.exception import CustomException
 from src.movies_recommendation.logger import logging
 import os
+from nltk.stem.porter import PorterStemmer
+
 
 @dataclass
 class DataTransformationConfig:
@@ -25,7 +29,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         if isinstance(X, np.ndarray):
-            X = pd.DataFrame(X, columns=self.columns)
+            X = pd.DataFrame(X)
         return X.drop(columns=self.columns)
 
 class DropDuplicates(BaseEstimator, TransformerMixin):
@@ -38,16 +42,35 @@ class DropDuplicates(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return X.drop_duplicates()
 
-class splitcate(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
+class CombineTextColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=None):
         self.columns = columns
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        for col in self.columns:
-            X[col] = X[col].str.split()
+        if isinstance(X, pd.DataFrame):
+            return X[self.columns].apply(lambda x: ' '.join(x.astype(str)), axis=1)
+        else:
+            df = pd.DataFrame(X, columns=self.columns)
+            return df.apply(lambda x: ' '.join(x.astype(str)), axis=1)
+
+class DateExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self, date_column):
+        self.date_column = date_column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X[self.date_column] = pd.to_datetime(X[self.date_column], errors='coerce')  # Handle invalid parsing
+        if X[self.date_column].isnull().all():
+            raise ValueError(f"Date column '{self.date_column}' could not be parsed into datetime.")
+        X['day'] = X[self.date_column].dt.day.fillna(-1).astype(int)  # Fill NaT with a placeholder value (e.g., -1)
+        X['month'] = X[self.date_column].dt.month.fillna(-1).astype(int)
+        X['year'] = X[self.date_column].dt.year.fillna(-1).astype(int)
+        X = X.drop(columns=[self.date_column])
         return X
 
 class FilterZeroRuntimeBudget(BaseEstimator, TransformerMixin):
@@ -60,58 +83,104 @@ class FilterZeroRuntimeBudget(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return X[~((X['runtime'] == 0) & (X['budget'] == 0))]
 
+
+
+class SimpleNLTKPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, use_stemming=False):
+        self.use_stemming = use_stemming
+        self.stemmer = PorterStemmer() if use_stemming else None
+
+    def preprocess(self, text):
+
+        tokens = text.lower().split()  
+        
+       
+        if self.use_stemming:
+            tokens = [self.stemmer.stem(word) for word in tokens]
+        
+        return ' '.join(tokens)
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return [self.preprocess(text) for text in X]
+
+ 
+ 
+
+
+class DenseTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        if isinstance(X, csr_matrix):
+            return X.toarray()
+        else:
+            return X
+
+class ConvertToString(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.astype(str)
+
 class DataTransformation:
     def __init__(self):
         self.data_tranformation_config = DataTransformationConfig()
 
     def get_data_transformer_object(self):
         try:
-            categorical_columns = ['title', 'status', 'original_language', 'original_title', 'overview',
-                                   'tagline', 'genres', 'production_companies', 'production_countries', 'spoken_languages',
-                                   'keywords']
+            categorical_columns = [
+                'status', 'original_language', 'original_title', 'overview',
+                'tagline', 'genres', 'production_companies', 'production_countries', 'spoken_languages',
+                'keywords'
+            ]
 
-            Numeric_columns = ['id', 'vote_average', 'vote_count', 'revenue', 'runtime', 'adult', 'popularity', 'budget']
+            numeric_columns = [
+                'id', 'vote_average', 'vote_count', 'revenue', 'runtime', 'adult', 'popularity', 'budget',
+                'year', 'month', 'day'
+            ]
 
             columns_drop = ['homepage', 'imdb_id', 'backdrop_path', 'poster_path']
             columns_split = ['genres', 'production_companies', 'production_countries', 'keywords']
 
             sub_pipeline_1 = Pipeline(steps=[
                 ('drop_duplicates', DropDuplicates()),
-                ('split_cate', splitcate(columns=columns_split)),
                 ('filter_zero_runtime_budget', FilterZeroRuntimeBudget()),
-                ('drop_columns', DropColumns(columns=columns_drop))
+                ('drop_columns', DropColumns(columns=columns_drop)),
+                ('date_extractor', DateExtractor(date_column='release_date'))
             ])
 
             categorical_pipeline = Pipeline(steps=[
-                ('fill_categorical_values', SimpleImputer(strategy='constant', fill_value='Unknown'))
+                ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),  
+                ('combine_text', CombineTextColumns(columns=categorical_columns)),
+                ('drop_columns', DropColumns(columns=categorical_columns)),
+                ('nltk_preprocessor', SimpleNLTKPreprocessor(use_stemming=True)),               
+                ('vectorizer', CountVectorizer(stop_words='english',max_features=500)),
+                ('to_dense', DenseTransformer())
             ])
+
             numeric_pipeline = Pipeline(steps=[
                 ('imputer', KNNImputer(n_neighbors=5)),
                 ('scaler', StandardScaler())
             ])
-            release_date_pipeline = Pipeline(steps=[
-                ('release_date', SimpleImputer(strategy='most_frequent'))
-            ])
 
             columns_transformer = ColumnTransformer(transformers=[
-                ('categorical_columns', categorical_pipeline, categorical_columns),
-                ('numeric_pipeline', numeric_pipeline, Numeric_columns),
-                ('release_date_pipeline', release_date_pipeline, ['release_date']),
+                ('categorical', categorical_pipeline, categorical_columns),
+                ('numeric', numeric_pipeline, numeric_columns),
             ], remainder='passthrough')
-
-            sub_pipeline_2 = Pipeline(steps=[
-                ('columns_transform_data', columns_transformer)
-            ])
 
             final_pipeline = Pipeline(steps=[
                 ('data_cleaning', sub_pipeline_1),
-                ('columns_transformation', sub_pipeline_2)
+                ('columns_transformation', columns_transformer)
             ])
 
-            logging.info(f"categorical columns: {categorical_columns}")
-            logging.info(f"Numeric columns: {Numeric_columns}")
-            logging.info(f"Columns split: {columns_split}")
-            logging.info(f"Columns drop: {columns_drop}")
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numeric columns: {numeric_columns}")
+            logging.info(f"Columns to drop: {columns_drop}")
 
             return final_pipeline
 
@@ -124,10 +193,54 @@ class DataTransformation:
             test_data = pd.read_csv(test_path)
 
             logging.info("Read the train data and test data files")
+
             preprocessing_obj = self.get_data_transformer_object()
 
-            train_data_arr = preprocessing_obj.fit_transform(train_data)
-            test_data_arr = preprocessing_obj.transform(test_data)
+            sub_pipeline_1 = Pipeline(steps=[
+                ('drop_duplicates', DropDuplicates()),
+                ('filter_zero_runtime_budget', FilterZeroRuntimeBudget()),
+                
+            ])
+            train_target_data_extraction=sub_pipeline_1.fit_transform(train_data)
+
+            test_target_data_extraction=sub_pipeline_1.transform(test_data)
+
+
+
+            target_columns = 'title'
+
+            input_features_train_df = train_data.drop(columns=target_columns, axis=1)
+            target_features_train_df = train_target_data_extraction[target_columns]
+
+            input_features_test_df = test_data.drop(columns=[target_columns], axis=1)
+            target_features_test_df = test_target_data_extraction[target_columns]
+
+            logging.info(f"Original train shape: {input_features_train_df.shape}")
+            logging.info(f"Original test shape: {input_features_test_df.shape}")
+
+            # Ensure alignment by resetting index
+            original_train_indices = input_features_train_df.index
+            original_test_indices = input_features_test_df.index
+
+            train_data_arr = preprocessing_obj.fit_transform(input_features_train_df)
+            test_data_arr = preprocessing_obj.transform(input_features_test_df)
+
+            # Ensure target columns align with transformed data
+
+            logging.info(f"Shape of train_data_arr: {train_data_arr.shape}")
+            logging.info(f"Shape of target_features_train_df: {target_features_train_df.shape}")
+            logging.info(f"Shape of test_data_arr: {test_data_arr.shape}")
+            logging.info(f"Shape of target_features_test_df: {target_features_test_df.shape}")
+            
+
+            
+
+           
+
+            
+            train_arr = np.c_[train_data_arr, target_features_train_df]
+            test_arr = np.c_[test_data_arr, target_features_test_df]
+
 
             logging.info("Saving preprocessing object")
 
@@ -139,5 +252,3 @@ class DataTransformation:
         except Exception as e:
             logging.error("Custom Exception occurred", exc_info=True)  # Use logging.error to capture traceback
             raise CustomException(e, sys)
-
-           
