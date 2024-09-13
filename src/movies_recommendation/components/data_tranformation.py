a13d15289321ff468ccdd1074c10fb7baa04640d
Amantyagidatascientist <tyagiaman3558@gmail.com>
2024-09-13 21:11:20 +0530

    this is the

--- src/movies_recommendation/components/data_tranformation.py
+++ src/movies_recommendation/components/data_tranformation.py
@@ -77,7 +77,7 @@ class FilterZeroRuntimeBudget(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
-    def fit(self,  X, y=None):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
